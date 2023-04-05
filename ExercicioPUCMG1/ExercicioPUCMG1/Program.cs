64814b7714906b12857d1b8e019c18c56e0e141b
Victor Moura Dubeux <adv.victordubeux@gmail.com>
2023-04-05 16:15:22 -0300

    Commit 3
    Utiliza-se comandos condicionais (if-else) e de repetição (while).

--- ExercicioPUCMG1/ExercicioPUCMG1/Program.cs
+++ ExercicioPUCMG1/ExercicioPUCMG1/Program.cs
@@ -7,17 +7,39 @@ namespace ExercicioPUCMG1
     {
         static void Main(string[] args)
         {
-            double salario, inss;
+            double salario, inss, outroValor;
 
             Console.Write("Por favor, digite o seu salário: R$");
-            salario = float.Parse(Console.ReadLine());
+            salario = double.Parse(Console.ReadLine());
 
             if (salario <= 1000.00)
                 inss = salario * 0.08;
             else
                 inss = salario * 0.1;
-            
+
             Console.WriteLine($"O valor do INSS a ser pago é R${inss.ToString("F2")}.");
+
+            //testando if, if-else, else, while
+
+            Console.ReadKey();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("Brincadeira: tente adivinhar o valor escolhido!");
+
+            Console.Write("Informe um número: ");
+            outroValor = double.Parse(Console.ReadLine());
+
+            while (outroValor != 1000.00)
+            {
+                if (outroValor < 1000.00)
+                    Console.WriteLine("É menor que esse.");
+                else
+                    Console.WriteLine("É maior que esse.");
+
+                Console.Write("Informe outro número: ");
+                outroValor = double.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Você acertou! Parabéns!");
         }
     }
 }
