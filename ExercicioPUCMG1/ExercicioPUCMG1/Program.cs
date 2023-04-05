a629e96dea47410ee59bfd374d9aaf4629f78466
Victor Moura Dubeux <adv.victordubeux@gmail.com>
2023-04-05 15:32:45 -0300

    Commit 2
    Finalizado o exercício proposto pelo professor.

---
+++ ExercicioPUCMG1/ExercicioPUCMG1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExercicioPUCMG1
+{
+    class Exercicio
+    {
+        static void Main(string[] args)
+        {
+            double salario, inss;
+
+            Console.Write("Por favor, digite o seu salário: R$");
+            salario = float.Parse(Console.ReadLine());
+
+            if (salario <= 1000.00)
+                inss = salario * 0.08;
+            else
+                inss = salario * 0.1;
+            
+            Console.WriteLine($"O valor do INSS a ser pago é R${inss.ToString("F2")}.");
+        }
+    }
+}
